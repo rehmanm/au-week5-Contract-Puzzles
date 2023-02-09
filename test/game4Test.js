ce540fc0bdd0043da5725d82a7c81f0036a3bd70
Muhammad Abdul Rehman <muhammad.abdulrehman@gmail.com>
2023-02-09 11:33:24 -0500

    Done

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,14 +6,21 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const signer = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+
+    const address1 = await signer.getAddress();
+    const address2 = await signer2.getAddress();
+
+    return { game, address1, address2, signer2 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, address1, address2, signer2 } = await loadFixture(deployContractAndSetVariables);
 
-    // nested mappings are rough :}
+    // nested mappings are rough :} 
+    await game.connect(signer2).write(address1);
 
-    await game.win();
+    await game.win(address2);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game5Test.js
+++ test/game5Test.js
@@ -5,15 +5,42 @@ describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
+    const accounts = await ethers.provider.getSigner(0);
 
-    return { game };
+    return { game, accounts };
+  }
+
+  async function getAddress() {
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    let address = threshold
+    while (true) {
+      const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      address = await wallet.getAddress();
+      if (address < threshold) {
+        return { wallet, address }
+      }
+    }
+
+
   }
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
+    const { wallet, address } = await getAddress();//get a random address below threshold
+    console.log(`get randome address${address}`)
+
+    //the random address does not have gas, send some gas to it:
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+    });
 
     // good luck
-
-    await game.win();
+    const tx = await game.connect(wallet).win();
+    console.log(`Transaction sent from address ${address}...`)
+    await tx.wait()
+    // await game.win();
+    console.log('Transaction completed')
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
